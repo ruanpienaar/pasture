096f549c2213478d98848092218195bd5e9cd735
ruanpienaar <ruan800@gmail.com>
2015-01-29 16:11:22 +0000

    ..

--- apps/pasture/src/pasture_db.erl
+++ apps/pasture/src/pasture_db.erl
@@ -22,7 +22,9 @@ init(MnesiaTbls,MasterNode) when MasterNode == node() ->
     [ExtraNodes] = [ Nodes -- [node()] ],
     ?INFO("Extra nodes : ~p\n\n",[ExtraNodes]),
     mnesia:change_config(extra_db_nodes, ExtraNodes),
-    stopped = mnesia:stop(),
+    lists:foreach(fun(SN) ->
+        stopped = rpc:call(SN, mnesia, stop, [])
+    end, ExtraNodes),
     ?INFO("Trying to install schema on ~p\n\n",[Nodes]),
     timer:sleep(2000),
     case mnesia:create_schema(Nodes) of
